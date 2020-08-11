0da497c1581a01f3ec60bf0e3df4fa6bba76678c
Meianna Oeser <meianna.oeser@gmail.com>
2020-08-10 19:29:44 -0700

    Updates

---
+++ precache-manifest.4120e1d41e935389401d9a52d7eae7d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa63f10cc898a737a9f07246784c59f",
+    "url": "/8-10-demo/index.html"
+  },
+  {
+    "revision": "246fe5c4ecd94dcfc841",
+    "url": "/8-10-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/8-10-demo/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/8-10-demo/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246fe5c4ecd94dcfc841",
+    "url": "/8-10-demo/static/js/main.eca0853f.chunk.js"
+  },
+  {
+    "revision": "aee1803b198d7699c835",
+    "url": "/8-10-demo/static/js/runtime-main.1fcb1961.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/8-10-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
